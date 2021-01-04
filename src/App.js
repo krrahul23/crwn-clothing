e566614da6e222eae2ba0f41337e9bf01920c4eb
krrahul23 <raulk023@gmail.com>
2021-01-04 13:55:27 +0530

    Added links and styled them

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import "./App.css";
 import HomePage from "./pages/homepage/homepage";
 import { Switch, Route } from "react-router-dom";
 import ShopPage from "./pages/shop/shop";
+import Header from "./components/header/header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route path="/" exact component={HomePage}></Route>
         <Route path="/shop" exact component={ShopPage}></Route>
